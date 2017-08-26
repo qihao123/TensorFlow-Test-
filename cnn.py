992e0a0a9a331730c81bb3b36cab95fd625b13b7
HAO Q <qihoo2017@gmail.com>
2017-08-26 20:52:46 +0800

    Update cnn.py
    3000步左右acu约等于1

--- cnn.py
+++ cnn.py
@@ -46,7 +46,7 @@ correct_perdiction = tf.equal(tf.argmax(y_conv,1),tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_perdiction,tf.float32))
 
 tf.global_variables_initializer().run()
-for i in range(20000):
+for i in range(2000):
     batch = mnist.train.next_batch(50)
     if i%100 == 0:
         train_accuracy = accuracy.eval(feed_dict={x:batch[0],y_:batch[1],keep_prop:1.0})
